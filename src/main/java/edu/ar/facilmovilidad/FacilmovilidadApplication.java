84d104533fc9a28f8dc52eccf1448e9bd73d4c68
Lorena-arr2025 <arrnan846@gmail.com>
2025-11-24 02:02:18 -0300

    Primer commit

---
+++ src/main/java/edu/ar/facilmovilidad/FacilmovilidadApplication.java
@@ -0,0 +1,13 @@
+package edu.ar.facilmovilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilmovilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilmovilidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ar/facilmovilidad/FacilmovilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ar.facilmovilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilmovilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
